987d11754652b914328d5d0df737552733ab566e
JoeBiden <121745705+Brain-410@users.noreply.github.com>
2025-04-15 20:40:36 +1000

    Added "About Me" Section
    Added "About Me" Section.
    Includes wavey border.
    NavBar to "About Me" section is functional

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ document.getElementById("scroll-link1").addEventListener("click", function(event
     document.getElementById("target-section1").scrollIntoView({ behavior: "smooth" });
 });
 
-console.log('Run')
+document.getElementById("scroll-link2").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("target-section2").scrollIntoView({ behavior: "smooth" });
+});
