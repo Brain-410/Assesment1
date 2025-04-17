2bde6c7a1357dfae558a27d0ed075ef2035eb205
JoeBiden <121745705+Brain-410@users.noreply.github.com>
2025-04-17 10:43:41 +1000

    Started Projects Section

--- script.js
+++ script.js
@@ -8,8 +8,12 @@ document.getElementById("scroll-link2").addEventListener("click", function(event
     document.getElementById("target-section2").scrollIntoView({ behavior: "smooth" });
 });
 
-
 document.getElementById("scroll-link3").addEventListener("click", function(event) {
     event.preventDefault();
     document.getElementById("target-section3").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("scroll-link4").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("target-section4").scrollIntoView({ behavior: "smooth" });
 });
