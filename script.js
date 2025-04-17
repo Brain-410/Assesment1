8fc0230f106896450b9da98419b0bc726cf74c71
JoeBiden <121745705+Brain-410@users.noreply.github.com>
2025-04-17 22:27:38 +1000

    Worked on "Projects" section
    Added content
    Added on scroll animation

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ document.getElementById("scroll-link4").addEventListener("click", function(event
     event.preventDefault();
     document.getElementById("target-section4").scrollIntoView({ behavior: "smooth" });
 });
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("in-view")
+            entry.target.classList.remove("not-in-view")
+        } else {
+            entry.target.classList.remove("in-view")
+            entry.target.classList.add("not-in-view")
+        }
+    })
+}, {
+    rootMargin: "0px",
+    threshold: [0, 0.1, 1]
+})
+
+const tags = document.querySelectorAll("div.Project_odd, div.Project_even, h2.title")
+
+tags.forEach((tag) => {
+    observer.observe(tag)
+})
